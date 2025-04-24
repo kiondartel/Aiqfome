314c8fbce6d0dd7b4af31da508ee4ab41272e961
Gabriel Lucas <gabriel.severino@base2.com.br>
2025-04-24 14:10:00 -0300

    feat(assets): add local food images and imageAssets map

---
+++ src/constants/imageAssets.ts
@@ -0,0 +1,9 @@
+export const images = {
+  pizza: require("../assets/images/pizza.jpg"),
+  burger: require("../assets/images/hambur.jpg"),
+  acai: require("../assets/images/acai.jpg"),
+  frango: require("../assets/images/frango.jpg"),
+  salada: require("../assets/images/salada.jpg"),
+  sushi: require("../assets/images/sushi.jpeg"),
+  tacos: require("../assets/images/tacos.jpg"),
+};
